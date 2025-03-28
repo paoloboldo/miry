0e54cfdd00620aae4801d7bf57332070d1d410f5
paolo <paoloboldo@gmail.com>
2025-03-28 16:56:47 +0100

    work in progress

--- generate_report.py
+++ generate_report.py
@@ -4,12 +4,35 @@ import pandas as pd
 
 file = r"assets\TURNI GIUGNO.xlsx"
 
-# workbook = openpyxl.Workbook()
-# sheet = workbook.active
+wb = openpyxl.load_workbook(file)
+sheet_names = wb.sheetnames
+weeks = []
+for name in sheet_names:
+    if "sett" in name:
+        weeks.append(name)
+weeks.sort()
 
+def complete_joint_cells(array):
+    for i, value in enumerate(array):
+        if "Unnamed" in value:
+            array[i] = array[i-1]
+    return array
+
+# sheet_name = "Sett
 df = pd.read_excel(file, sheet_name="Foglio3")
-col_1 = df.columns
-col_2 = df.values[0]
-col_3 = df.values[1]
-orari = df.values[:,0]
+month_day = complete_joint_cells(df.columns.to_numpy()[1:])  # giorno del mese
+values = df.values.to_numpy()
+week_day = complete_joint_cells(values[0, 1:])  # giorno della settimana
+worker = values[1, 1:]  # nome dipendente
+times = values[:,0]
+first_t = None
+last_t = None
+for t, time in enumerate(times):
+    if "Unnamed" not in time and start_time is None:
+        start_time = t
+        continue
+    if "Unnamed" not in time and start_time is None:
+
+
+
 
