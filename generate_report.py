051b2a0fd73661d485cb174adb641f27eb7ba4a9
paolo <paoloboldo@gmail.com>
2025-03-28 18:43:11 +0100

    read week matrix

--- generate_report.py
+++ generate_report.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 import openpyxl
 import pandas as pd
 
@@ -14,25 +14,43 @@ weeks.sort()
 
 def complete_joint_cells(array):
     for i, value in enumerate(array):
-        if "Unnamed" in value:
+        if str(value) == "nan":
             array[i] = array[i-1]
     return array
 
+
+def first_last_indices(array):
+    first_i = None
+    last_i = None
+    for i, value in enumerate(array):
+        if str(value) != "nan":
+            last_i = i
+            if first_i is None:
+                first_i = i
+            continue
+        else:
+            if first_i is not None:
+                break
+    return [first_i, last_i]
+
+
 # sheet_name = "Sett
 df = pd.read_excel(file, sheet_name="Foglio3")
-month_day = complete_joint_cells(df.columns.to_numpy()[1:])  # giorno del mese
-values = df.values.to_numpy()
-week_day = complete_joint_cells(values[0, 1:])  # giorno della settimana
-worker = values[1, 1:]  # nome dipendente
-times = values[:,0]
-first_t = None
-last_t = None
-for t, time in enumerate(times):
-    if "Unnamed" not in time and start_time is None:
-        start_time = t
-        continue
-    if "Unnamed" not in time and start_time is None:
+values = df.values
 
+worker = values[1]  # nome dipendente
+first_w, last_w = first_last_indices(worker)
+worker = worker[first_w: last_w + 1]
+
+month_day = complete_joint_cells(df.columns.to_numpy()[first_w: last_w + 1])  # giorno del mese
+
+week_day = complete_joint_cells(values[0, first_w: last_w + 1])  # giorno della settimana
+
+times = values[:,0]
+first_t, last_t = first_last_indices(times)
+times = times[first_t: last_t + 1]
+
+values = values[first_t: last_t + 1, first_w: last_w + 1]
 
 
 
